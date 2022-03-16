667a740683e1267c45c1d145b5e24c4e5434e99f
Ehua <52437023+EhuaNetwork@users.noreply.github.com>
2022-03-16 21:28:05 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,56 @@
+<?php
+
+function get_config()
+{
+    return $config = [
+        //官网地址
+        'base_url' => "http://www.711688.net.cn/",
+        'database'=>[
+            // 数据库连接地址
+            'hostname' => "127.0.0.1",
+            // 数据库名称
+            'dbname'   => "ehua_711688caiji",
+            // 数据库账户
+            'username' => "ehua_711688caiji",
+            // 数据库密码
+            'password' => "ehua_711688caiji",
+            // 数据库表前缀
+            'prefix'     => "cj_",
+            'resultset_type' => false,
+        ],
+        'auth'=>[
+            0=>[
+//                'url'=>'http://mdyk.cnshangji.cn/',// 玛蒂拉克  必须以/结尾
+//                'name'=>'madiyake',
+//                'pass'=>'madiyake123',
+
+//                'url'=>'http://kunsp.cnshangji.cn/',// 昆嵛正源食品  必须以/结尾
+//                'name'=>'zhengyuanshipin',
+//                'pass'=>'zhengyuan123',
+
+//                'url'=>'http://sdse.cnshangji.cn/',// 诗丹塞尔  必须以/结尾
+//                'name'=>'shidansaier',
+//                'pass'=>'shidansaier1234',
+
+//                'url'=>'http://zysm.cnshangji.cn/',// 中源商贸  必须以/结尾
+//                'name'=>'zhongyuanshangmao',
+//                'pass'=>'zhongyuanshangmao123',
+
+//                'url'=>'http://zcgs.cnshangji.cn/',// 众成果蔬  必须以/结尾
+//                'name'=>'ytzcgs',
+//                'pass'=>'ytzcgs123',
+
+
+//                'url'=>'http://ksny.cnshangji.cn/',// 艮水农业  必须以/结尾
+//                'name'=>'genshui',
+//                'pass'=>'genshui123',
+
+//                'url'=>'http://wchc.cnshangji.cn/',// 文萃画材  必须以/结尾
+//                'name'=>'wencui',
+//                'pass'=>'wencui123',
+
+
+            ]
+        ]
+    ];
+}
